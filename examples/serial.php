1f001017f09996144daf5ff960bc087d4bb60a45
simon <simon@yeacreate.com>
2020-05-28 12:20:44 +0800

    Update serial.php

--- examples/serial.php
+++ examples/serial.php
@@ -5,19 +5,23 @@
 $got=0;
 wiringPiSetup();
 $fd = serialOpen("/dev/ttyS1",9600);
-
+$message = "";
 
 for (;;){
     if (serialDataAvail($fd))   //recive message
-	{
-    	echo serialGetchar($fd);
-	$got = 1;
-	}
+        {
+        //echo 
+        $message .= bin2hex(chr(serialGetchar($fd)));  //got by hex
+        $got = 1;
+        }
    if($got==1){
-	serialPrintf($fd,"got it");	//sé//sent message
-	serialPutchar($fd,"49"); 	//sent message with hex
-	$got=0;
-	}
+        #serialPrintf($fd,"got it");    //sent message
+        serialPuts($fd,hex2bin($message));      //sent message with hex
+        #serialPutchar($fd,'0x5a'); //sent message with hex
+        $got=0;
+        echo "$message \n";
+        $message = "";
+        }
 
     usleep(5000);
 }
